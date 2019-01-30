aa360aafee791a3dfee7588851dc4aa74b62e81d
your name <your email>
2019-01-30 17:23:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Spin size="large" fix v-if="spinShow">
+      <Icon type="ios-loading" size="18" class="spin-icon-load"></Icon>
+      <div>Loading</div>
+    </Spin>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      spinShow: true
+    };
+  }
+};
+</script>
+
+<style>
+.spin-icon-load {
+  animation: ani-spin 1s linear infinite;
+}
+@keyframes ani-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ src/plugins/iview.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { Button, Spin, Icon } from 'iview'
+
+Vue.component('Button', Button)
+Vue.component('Spin', Spin)
+Vue.component('Icon', Icon)
+
+import 'iview/dist/styles/iview.css'
